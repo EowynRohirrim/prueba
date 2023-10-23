6f4baff013cdaca01f09f8e931e37d0055a56454
EowynRohirrim <145437880+EowynRohirrim@users.noreply.github.com>
2023-10-23 18:06:25 +0200

    Initial Commit

--- app/src/main/java/com/patri/aplicacionespmdmt2/BoardgamesApp/BoardGameActivity.kt
+++ app/src/main/java/com/patri/aplicacionespmdmt2/BoardgamesApp/BoardGameActivity.kt
@@ -10,6 +10,9 @@ class BoardGameActivity : AppCompatActivity() {
     private lateinit var rvCategories: RecyclerView
     private lateinit var rvGames: RecyclerView
 
+    private lateinit var fabAddGame: FloatingActionButton
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_board_game)
@@ -19,6 +22,41 @@ class BoardGameActivity : AppCompatActivity() {
     }
 
     private fun initComponents(){
-        rvCategories = findViewById<RecyclerView>(R.id.)
+
+        rvCategories = findViewById(R.id.rvCategories)
+        rvGames = findViewById(R.id.rvGames)
+        fabAddGame = findViewById(R.id.fabAddGame)
+
     }
+    private fun initListeners() { fabAddGame.setOnClickListener{ showDialog() }
+    }
+
+
+
+    private fun showDialog() {
+        val dialog = Dialog(this)
+        dialog.setContentView(R.layout.dialog_game)
+
+        val btnAddGame: Button = dialog.findViewById(R.id.btnAddGame)
+        val etGame: EditText = dialog.findViewById(R.id.etGame)
+        val rgCategories: RadioGroup = dialog.findViewById(R.id.rgCategories)
+
+        dialog.show()
+    }
+
+
+    fun render(game: Game) {
+        tvGame.text = game.name
+
+        val color = when(game.category){
+            GameCategory.Cooperative -> R.color.bgapp_cooperative_category
+            GameCategory.Deckbuilding -> R.color.bgapp_deckbuilding_category
+            GameCategory.Euro -> R.color.bgapp_euro_category
+            GameCategory.LCG -> R.color.bgapp_lcg_category
+            GameCategory.Legacy -> R.color.bgapp_legacy_category
+        }
+
+        cbGame.buttonTintList = ColorStateList.valueOf(ContextCompat.getColor(cbGame.context, color))
+    }
+
 }
