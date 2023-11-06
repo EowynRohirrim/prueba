5e46c4972f38b42d0ca3b4941094670914e880b9
EowynRohirrim <145437880+EowynRohirrim@users.noreply.github.com>
2023-11-06 18:10:50 +0100

    Initial Commit

--- app/src/main/java/com/patri/aplicacionespmdmt2/ColorsApp/Colors.kt
+++ app/src/main/java/com/patri/aplicacionespmdmt2/ColorsApp/Colors.kt
@@ -1,4 +1,14 @@
 package com.patri.aplicacionespmdmt2.ColorsApp
 
+import androidx.recyclerview.widget.RecyclerView
+import com.patri.aplicacionespmdmt2.R
+
 class Colors {
+    private lateinit var rvColorsH: RecyclerView
+
+    private fun initcomponents() {
+        rvColorsH = findViewById<RecyclerView>(R.id.rvColorsH)
+    }
+
+
 }
